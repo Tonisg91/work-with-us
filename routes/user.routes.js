fb4dddf583ca3c33fa65018c7a239d2833b47ba1
Toni <sancheztoni.garcia@gmail.com>
2020-06-14 13:35:10 +0200

    pagina user-profile y ruta

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-/* GET My account */
 router.get("/myaccount", (req, res, next) => {
   res.render("user/my-account", { currentUser: req.session.currentUser });
 });
