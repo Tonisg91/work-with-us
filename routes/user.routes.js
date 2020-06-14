450fc3bc001edb3747b76d11d110cda814649611
Toni <sancheztoni.garcia@gmail.com>
2020-06-14 14:15:16 +0200

    my account ruta post, view y editar modelo

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,8 +1,17 @@
 const express = require("express");
+const User = require("../models/Users.model");
 const router = express.Router();
 
 router.get("/myaccount", (req, res, next) => {
-  res.render("user/my-account", { currentUser: req.session.currentUser });
+  const user = req.session.currentUser;
+  res.render("user/my-account", { user });
+});
+
+router.post("/editUser", async (req, res, next) => {
+  const userId = req.session.currentUser._id;
+  console.log(req.body);
+  await User.findByIdAndUpdate(userId, req.body);
+  res.redirect("/myaccount");
 });
 
 module.exports = router;
