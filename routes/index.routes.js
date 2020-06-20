2e84d3a096cc758877d23fab1c4b736f622f5ed2
Toni <sancheztoni.garcia@gmail.com>
2020-06-20 15:04:42 +0200

    Falta revisar vistas

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,15 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const {
-  getIndex,
-  postIndex,
-  getUserProfile,
-} = require("../controllers/index.controllers");
+const C = require('../controllers/index.controllers')
+
 
 /* GET home page */
 router
-  .get("/", getIndex)
-  .post("/", postIndex)
-  .get("/userProfile/:id", getUserProfile)
+  .get("/", C.getIndex)
+  .post("/", C.postIndex)
+  .get("/userProfile/:id", C.getUserProfile)
 
 module.exports = router;
