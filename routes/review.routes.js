4d02a4464c8c77effc28a360cbf78bfef443878f
Toni <sancheztoni.garcia@gmail.com>
2020-06-20 13:56:58 +0200

    listado anuncios sin anuncios propios y refactoring controllers

--- routes/review.routes.js
+++ routes/review.routes.js
@@ -1,30 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const User = require('../models/Users.model');
-const Review = require('../models/Review.model');
 
+const { postAddReview, getAddReview } = require("../controllers/review.controllers");
 
-router.get('/addreview/:professionalId', async (req, res, next) => {
-  try {
-    const currentUser = req.session.currentUser;
-    const { professionalId } = req.params;
-    res.render('reviews/add-review', { currentUser, professionalId });
-  } catch (error) {
-    next(error)
-  }
-})
-
-router.post('/addreview/:professionalId', async (req, res, next) => {
-  try {
-    const { professionalId, title, description, rating } = req.body;
-    const newReview = await Review.create({ professionalId, title, description, rating });
-    await User.findByIdAndUpdate(professionalId, { $push: { reviews: newReview._id } });
-    res.redirect('/myaccount');
-  } catch (error) {
-    next(error)
-  }
-})
-
-
+router
+  .get('/addreview/:professionalId', getAddReview)
+  .post('/addreview/:professionalId', postAddReview)
 
 module.exports = router;
