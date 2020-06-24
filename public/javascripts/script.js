2df5707217dc4ab9c5b8ac1a644e4cd97983f04e
Toni <sancheztoni.garcia@gmail.com>
2020-06-24 22:38:00 +0200

    Filtro y funciones genericas. Falta colocar diferentes tipos de filtro en hbs

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,3 +1,6 @@
+// TOOLS
+const capitalize = (str) => str.toLowerCase().charAt(0).toUpperCase() + str.slice(1);
+
 //SIDENAV
 function openNav() {
   document.getElementById("sidenav").style.width = "50%";
@@ -5,10 +8,27 @@ function openNav() {
 function closeNav() {
   document.getElementById("sidenav").style.width = "0";
 }
+
 //FILTERS
 const cityFilterInput = document.querySelector('#tag-filter');
-const cityFilterBtn = document.querySelector('#tag-filter-btn')
+const cityFilterBtn = document.querySelector('#tag-filter-btn');
+const announces = document.querySelectorAll('.announce-card');
 
+const clearList = arr => arr.forEach(e => e.classList.remove('hidden'));
+
+const filterList = (arr, val, announcesArr) => {
+  val ? hideElements(arr, val) : clearList(announcesArr)
+}
+
+const hideElements = (arr, val) => {
+  arr.forEach((e, idx) => {
+    if (e.innerHTML != val) {
+      announces[idx].classList.add('hidden')
+    }
+  })
+}
 cityFilterBtn.addEventListener('click', () => {
-  console.log(cityFilterVal.value)
+  const val = capitalize(cityFilterInput.value);
+  const citiesOfAnnouncements = document.querySelectorAll('.city');
+  filterList(citiesOfAnnouncements, val, announces);
 })
