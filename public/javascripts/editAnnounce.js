33af162b35a1144ed56965559e0fe735c7359aa1
Toni <sancheztoni.garcia@gmail.com>
2020-07-02 12:50:20 +0200

    solucionado error estilos de  editar info anuncio

--- public/javascripts/editAnnounce.js
+++ public/javascripts/editAnnounce.js
@@ -1,7 +1,12 @@
 const editFunction = () => {
   const className = 'hidden'
+  const form = document.getElementById('edit-announce-data');
+  const editBtn = document.getElementById('edit-announcement');
+
   document.getElementById('announce-data').classList.add(className);
-  document.getElementById('edit-announce-data').classList.remove(className);
-  document.getElementById('edit-announcement').classList.add(className)
   document.getElementById('save-button').classList.remove(className)
+  form.classList.remove(className);
+  editBtn.classList.add(className);
+  //Ponemos los estilos aqui porque al introducirlo desd el scss, da problemas.
+  form.style = 'display: flex; flex-direction: column;'
 }
